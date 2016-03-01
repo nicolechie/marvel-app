cd2ba66776cf8482c126aee9b4353342475cb41c
nicolechie <nicolechie@mac.com>
2016-03-01 12:59:06 -0700

    added search

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
   $.getJSON('http://gateway.marvel.com:80/v1/public/characters?apikey=fe53b28e7625459833f259efa7324a60', function(data){
     console.log(data);
+
+    function showResults(items) {
+        $('#search-results').html("");
+		for (var i = 0; i < items.length; i++) {
+            var videoThumbnail = items[i].results.thumbnail.path + 'portrait_small.jpg';
+            $('#search-results').append('<li><img src =' + videoThumbnail + '></li>');
+    	}
+    }
+    $(function(){
+        $('#search-term').submit(function(e) {
+            var searchTerm = $('#query').val();
+            e.preventDefault();
+            getRequest(searchTerm);
+        });
+    })
   });
 
-function getInspiration(answerers) {
+function getRequest(searchTerm) {
 	// the parameters we need to pass in our request to StackOverflow's API
 	var request = { 
     apikey: fe53b28e7625459833f259efa7324a60
@@ -15,8 +30,8 @@ function getInspiration(answerers) {
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET",
 	})
-		.done(function(result){ //this waits for the ajax to return with a succesful promise object
-		    console.log(result);
+		.done(function(data){ //this waits for the ajax to return with a succesful promise object
+		    console.log(data);
 		})
 	};
 });
